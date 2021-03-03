13bfd54e5f8e2ae1f982f4612f5740f655deeaff
4scencio <alessandroascencio@protonmail.com>
2021-03-03 11:32:53 -0800

    base of the project

---
+++ pages/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="index">
+    <h1>Página Inicial :)</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      title: 'Página Inicial'
+    }
+  },
+  head(this:any) {
+    return {
+      title: this.title
+    }
+  }
+})
+</script>
+
+<style>
+
+.index {
+  margin: 0 auto;
+}
+
+a.nuxt-link-exact-active {
+  font: bold;
+  color: green;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ pages/sobre.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <h1>Página Sobre :)</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      title: 'Página Sobre'
+    }
+  },
+  head(this:any) {
+    return {
+      title: this.title
+    }
+  }
+})
+</script>
+
+<style scoped>
+a.nuxt-link-exact-active {
+  font: bold;
+  color: green;
+}
+</style>
